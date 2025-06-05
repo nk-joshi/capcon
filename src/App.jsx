c498cc64b6ac477df7f02184f1e827f029905935
joshi.nikhil <nikhiljoshi1609@gmail.com>
2025-06-05 19:14:22 +0530

    code bettered

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import Header from "./components/Header";
 import Body from "./components/Body";
+import HeaderIcon from "./assets/header.svg";
 
 function App() {
   return (
     <>
-      <Header />
+      <img fetchPriority="high" src={HeaderIcon} alt="header image" />
       <Body />
     </>
   );
